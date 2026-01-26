d99ec3e53b9a44c9c03227eb7e7f88cb02b6cb52
Aleksandr Mikhailov <dev@avidclam.com>
2026-01-26 23:09:24 +0100

    Exclude get_path_values from __init__.py

--- dotview/__init__.py
+++ dotview/__init__.py
@@ -1,7 +1,8 @@
 from .na import NA
 from .treeview import TreeView
 from .dotview import DotView
-from .jsonpath import get_path_values
+# Don't import jsonpath_ng by default. Instead, use:
+# from dotview.jsonpath import get_path_values
 
 
-__all__ = ['NA', 'TreeView', 'DotView', 'get_path_values']
+__all__ = ['NA', 'TreeView', 'DotView']
